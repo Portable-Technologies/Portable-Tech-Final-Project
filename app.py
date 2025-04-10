3834e165a22c9f6f8f31f00addb951265816f37d
Tharuka Dananjana <32910424+tharuka95@users.noreply.github.com>
2025-04-10 14:28:18 -0400

    Update app.py to support  s3 bucket

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ DBPWD = os.environ.get("DBPWD") or "passwors"
 DATABASE = os.environ.get("DATABASE") or "employees"
 COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
 DBPORT = int(os.environ.get("DBPORT"))
+s3=os.environ.get("s3url")
 
 # Create a connection to the MySQL database
 db_conn = connections.Connection(
@@ -47,11 +48,11 @@ COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lim
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('addemp.html', color=color_codes[COLOR])
+    return render_template('addemp.html', color=color_codes[COLOR], image=s3)
 
 @app.route("/about", methods=['GET','POST'])
 def about():
-    return render_template('about.html', color=color_codes[COLOR])
+    return render_template('about.html', color=color_codes[COLOR], image=s3)
     
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
@@ -75,11 +76,11 @@ def AddEmp():
         cursor.close()
 
     print("all modification done...")
-    return render_template('addempoutput.html', name=emp_name, color=color_codes[COLOR])
+    return render_template('addempoutput.html', name=emp_name, color=color_codes[COLOR], image=s3)
 
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
-    return render_template("getemp.html", color=color_codes[COLOR])
+    return render_template("getemp.html", color=color_codes[COLOR], image=s3)
 
 
 @app.route("/fetchdata", methods=['GET','POST'])
@@ -108,7 +109,7 @@ def FetchData():
         cursor.close()
 
     return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],
-                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], color=color_codes[COLOR])
+                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], color=color_codes[COLOR], image=s3)
 
 if __name__ == '__main__':
     
